c7059e05d7b7a0a5f9bd68b8793de2be3d656082
Olisanna <amaechiemeka123@gmail.com>
2022-07-11 14:20:51 -0700

    kjjnjn

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,7 +2,7 @@
 
 /**
  * main - assign a random mumber to the varible n
- * each time it gets executed and print the last 
+ * each time it gets executed and print the last
  * digit of the number stored in the variable n
  * Return: Always 0 (Success)
  */
